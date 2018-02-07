a41fb069fe3326bb8e1cc64c42bd172c5966096e
Maurice Bertram <Stricker78@live.de>
2018-02-07 10:32:57 +0100

    First Update

---
+++ index.php
@@ -0,0 +1,35 @@
+<!--
+//	Chatbox - Coding challenge by DreamGamer
+//	Project start: 07.02.2018	
+//	
+//	Coded by: Maurice Bertram
+-->
+
+<html>
+<head>
+	<title>Chatbot - Coding Challenge</title>
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"> 
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<script type="text/javascript" src="/js/jquery.js"></script>
+	<script type="text/javascript" src="/js/bootstrap.js"></script>
+	<script type="text/javascript" src="/js/main.js"></script>
+</head>
+<body>
+
+	<div class="main-container">
+		<div class="text-container">
+			<p>Nutzer: HALLO</p>
+			<br />
+			<p>Ich: HALLO</p>
+			<br />
+			<p>Nutzer: HALLO</p>
+			<br />
+			<p>Ich: HALLO</p>
+			<br />
+			<p>Nutzer: HALLO</p>
+		</div>
+	</div>
+
+</body>
+</html>
